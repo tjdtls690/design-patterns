12da6267cb49f32f2f0821e41248871e773a37b5
JunH <tjdtls690@gmail.com>
2022-08-25 23:54:54 +0900

    :art: refactor : static inner holder class 를 통해 싱글톤 보호하는 학습 테스트 구현

--- src/main/java/me/whiteship/designpatterns/_01_creational_patterns/_01_singleton/App.java
+++ src/main/java/me/whiteship/designpatterns/_01_creational_patterns/_01_singleton/App.java
@@ -5,18 +5,35 @@ import java.io.*;
 public class App {
 
     public static void main(String[] args) throws IOException, ClassNotFoundException {
-        Settings5 settings = Settings5.INSTANCE;
-
-        Settings5 settings1 = null;
+        // enum
+//        Settings5 settings = Settings5.INSTANCE;
+        
+        // static inner holder class
+        Settings4 settings = Settings4.getInstance();
+        
+        // enum
+        // 1. 직렬화 역직렬화로부터 싱글톤을 보호한다.
+        // 2. 리플랙션의 Constructor 의 사용을 애초에 막아준다.
+        
+        // static inner holder class
+        // 1. 직렬화, 역직렬화할 때 사용되는 protected Object readResolve() 메서드를
+        //    재정의 해줌으로써 싱글톤을 보호한다.
+        // 2. 그러나 리플렉션까지 막지는 못한다.
+        
+        // 가장 안전한 방법은 enum
+        // 그러나 상속을 받아야하거나 지연초기화를 사용해야할 때는 static inner holder class
+        
+        
+        // 직렬화 역직렬화 테스트
+        Settings4 settings1 = null;
         try (ObjectOutput out = new ObjectOutputStream(new FileOutputStream("settings.obj"))) {
             out.writeObject(settings);
         }
-
+        
         try (ObjectInput in = new ObjectInputStream(new FileInputStream("settings.obj"))) {
-            settings1 = (Settings5) in.readObject();
+            settings1 = (Settings4) in.readObject();
         }
 
         System.out.println(settings == settings1);
     }
-
 }
--- src/main/java/me/whiteship/designpatterns/_01_creational_patterns/_01_singleton/Settings4.java
+++ src/main/java/me/whiteship/designpatterns/_01_creational_patterns/_01_singleton/Settings4.java
@@ -1,9 +1,11 @@
 package me.whiteship.designpatterns._01_creational_patterns._01_singleton;
 
+import java.io.Serializable;
+
 /**
  * static inner 클래스 홀더
  */
-public class Settings4 {
+public class Settings4 implements Serializable {
 
     private Settings4() { }
 
@@ -14,5 +16,11 @@ public class Settings4 {
     public static Settings4 getInstance() {
         return Settings4Holder.INSTANCE;
     }
-
+    
+    // 직렬화, 역직렬화를 할 때 사용하는 메서드이므로
+    // 이 메서드를 이렇게 재정의 해주면
+    // 직렬화를 통해 새로운 객체를 만드려 해도 싱글톤을 유지할 수 있다.
+    protected Object readResolve() {
+        return getInstance();
+    }
 }
